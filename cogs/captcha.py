2f40bd5ec6b3b90cc9b9eaa9e0a84ed37f7cfb12
Ninutes <zh4creative@gmail.com>
2024-02-08 11:27:34 +0700

    Changes to be committed: 	modified:   cogs/captcha.py

--- cogs/captcha.py
+++ cogs/captcha.py
@@ -54,7 +54,6 @@ class Captcha(commands.Cog):
                 if GLOBAL.get_value('autosolve'):
                     return await self.get_result(msg, self.captcha_image, self.captcha_length)
                 return await LOG.captcha(msg, 'detected')
-            return await LOG.captcha(msg, 'detected')
     
     async def get_result(self, message : selfcord.Message, image, length):
         solve_time = time()
